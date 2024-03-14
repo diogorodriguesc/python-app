ea789fb9fac6a30c0bfc1e7114e4bb6a3b011838
Diogo Correia <diogorodriguescorreia89@gmail.com>
2024-03-14 23:34:06 +0000

    Decouple logic from Database Migrations.

--- src/migrations/database_migrations.py
+++ src/migrations/database_migrations.py
@@ -5,12 +5,20 @@ import importlib.util
 from logger.logger_interface import LoggerInterface
 
 
+def load_module(filename: str, location) -> object:
+    spec = importlib.util.spec_from_file_location(filename, location)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+
+    return getattr(module, filename[:-3])()
+
+
 class DatabaseMigrations:
-    __connection = None
+    __repository = None
     __logger: LoggerInterface
 
     def __init__(self, host: str, port: int, database: str, user: str, password: str, logger: LoggerInterface) -> None:
-        self.__conn = psycopg2.connect(
+        self.__repository = MigrationsRepository(
             database=database,
             user=user,
             host=host,
@@ -20,7 +28,7 @@ class DatabaseMigrations:
         self.__logger = logger
 
     def migrate(self):
-        self.__create_migrations_table_if_not_exists()
+        self.__repository.create_migrations_table_if_not_exists()
 
         for filename in sorted(os.listdir("../migrations")):
             if filename.endswith(".py"):
@@ -29,22 +37,28 @@ class DatabaseMigrations:
     def __migrate_file(self, filename: str) -> None:
         self.__logger.info(f"Migrating {filename}...")
 
-        if self.__check_if_migrated(filename):
+        if self.__repository.check_if_migrated(filename):
             self.__logger.info(f"{filename} is migrated")
         else:
             self.__logger.info(f"{filename} is not migrated, migration will take place")
-            self.__insert_migration(filename)
-            obj = self.__load_module(filename, f"../migrations/{filename}")
+            self.__repository.insert_migration(filename)
+            obj = load_module(filename, f"../migrations/{filename}")
             obj.up()
 
-    def __load_module(self, filename: str, location) -> object:
-        spec = importlib.util.spec_from_file_location(filename, location)
-        module = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(module)
 
-        return getattr(module, filename[:-3])()
+class MigrationsRepository:
+    __connection = None
 
-    def __check_if_migrated(self, filename: str) -> bool:
+    def __init__(self, host: str, port: int, database: str, user: str, password: str):
+        self.__conn = psycopg2.connect(
+            database=database,
+            user=user,
+            host=host,
+            password=password,
+            port=port
+        )
+
+    def check_if_migrated(self, filename: str) -> bool:
         cursor = self.__conn.cursor()
         cursor.execute(f"""SELECT * FROM migrations WHERE file = '{filename}'""")
         results = cursor.rowcount
@@ -54,14 +68,14 @@ class DatabaseMigrations:
 
         return True
 
-    def __insert_migration(self, filename: str) -> None:
+    def insert_migration(self, filename: str) -> None:
         self.__conn.cursor().execute(f"""
         INSERT INTO migrations (file) VALUES ('{filename}')
                         """)
 
         self.__conn.commit()
 
-    def __create_migrations_table_if_not_exists(self) -> None:
+    def create_migrations_table_if_not_exists(self) -> None:
         self.__conn.cursor().execute("""CREATE TABLE IF NOT EXISTS migrations(
             file VARCHAR (200) UNIQUE NOT NULL);
             """)
